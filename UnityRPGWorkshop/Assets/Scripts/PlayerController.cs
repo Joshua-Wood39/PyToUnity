0e723c37d8cbd2d62fb090631fc2eb791f5c1a4a
Joshua Wood <JerichoWall@apu.edu>
2020-06-16 16:02:17 -0700

    Expanding assets, adding collisions for map changes

--- UnityRPGWorkshop/Assets/Scripts/PlayerController.cs
+++ UnityRPGWorkshop/Assets/Scripts/PlayerController.cs
@@ -9,10 +9,17 @@ public class PlayerController : MonoBehaviour {
 
     public Animator myAnim;
 
+    public static PlayerController instance;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        if(instance == null) {
+            instance = this;
+        } else {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
